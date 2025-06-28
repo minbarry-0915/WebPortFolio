a38b394ea0a85c763fba109b7cde9e3a43a515b0
leejimin <jimin980915@naver.com>
2025-06-28 20:35:13 +0900

    fix: 미완료 작업 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Header from './_components/Header';
 import AwardSection from './_sections/AwardSection';
 import EducationSection from './_sections/EducationSection';
-import IntroduceSection from './_sections/IntroduceSection';
 import MainSection from './_sections/MainSection';
 import ProjectDetailSection from './_sections/ProjectDetailSection';
 import SkillSection from './_sections/SkillSection';
@@ -11,7 +10,6 @@ export default function Home() {
 	return (
 		<main className='flex flex-col items-center'>
 			<MainSection />
-			<IntroduceSection />
 			<Header />
 			<SkillSection />
 			<WorkExperienceSection />
